14764b078a7bd4a7fa33a42e71fdf2c51f6a8023
semyon422 <semyon422@users.noreply.github.com>
2021-07-04 22:18:54 +0500

    Implement part of community settings

--- controllers/community/init.lua
+++ controllers/community/init.lua
@@ -8,4 +8,17 @@ community_c.GET = function(params)
 	return 200, {community = community_entry}
 end
 
+community_c.PATCH = function(params)
+	local community_entry = communities:find(params.community.id)
+
+	community_entry.name = params.community.name
+	community_entry.alias = params.community.alias
+	community_entry.short_description = params.community.short_description
+	community_entry.description = params.community.description
+
+	community_entry:update("name", "alias", "short_description", "description")
+
+	return 200, {community = community_entry}
+end
+
 return community_c
