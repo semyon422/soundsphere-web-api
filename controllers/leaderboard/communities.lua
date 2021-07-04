53a610266ba618e93d06b22664828bc20f964b8f
semyon422 <semyon422@users.noreply.github.com>
2021-07-04 23:10:41 +0500

    Fix leaderboard communities returned table

--- controllers/leaderboard/communities.lua
+++ controllers/leaderboard/communities.lua
@@ -7,12 +7,17 @@ leaderboard_communities_c.GET = function(params)
     local sub_leaderboard_communities = community_leaderboards:find_all({params.leaderboard_id}, "leaderboard_id")
 	preload(sub_leaderboard_communities, "leaderboard", "community")
 
+	local communities = {}
+	for _, community_leaderboard in ipairs(sub_leaderboard_communities) do
+		table.insert(communities, community_leaderboard.community)
+	end
+
 	local count = community_leaderboards:count()
 
 	return 200, {
 		total = count,
 		filtered = count,
-		communities = sub_leaderboard_communities
+		communities = communities
 	}
 end
 
