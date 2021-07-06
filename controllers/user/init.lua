d54d324a98b2f52b055472b45bea824bc4bc8256
semyon422 <semyon422@users.noreply.github.com>
2021-07-06 23:40:46 +0500

    Add user patch method

--- controllers/user/init.lua
+++ controllers/user/init.lua
@@ -10,7 +10,19 @@ user_c.GET = function(params)
         name = user.name,
         tag = user.tag,
         latest_activity = user.latest_activity,
+        description = user.description,
     }
+
+	return 200, {user = user}
+end
+
+user_c.PATCH = function(params)
+	local user = Users:find(params.user_id)
+
+	user.name = params.user.name
+	user.description = params.user.description
+
+	user:update("name", "description")
 
 	return 200, {user = user}
 end
