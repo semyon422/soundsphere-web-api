d9959702ffc3bc64b43be1786427cbbb80aea99b
semyon422 <semyon422@users.noreply.github.com>
2021-12-18 16:04:39 +0500

    Add more default objects for easier testing

--- app.lua
+++ app.lua
@@ -162,7 +162,15 @@ app:match("/api/create_db", function(self)
 	local bcrypt = require("bcrypt")
 
 	local Users = require("models.users")
-	Users:create({
+	local Communities = require("models.communities")
+	local Leaderboards = require("models.leaderboards")
+	local Community_users = require("models.community_users")
+	local Community_leaderboards = require("models.community_leaderboards")
+	local Difftables = require("models.difftables")
+	local Roles = require("enums.roles")
+	local leaderboard_c = require("controllers.leaderboard")
+
+	local user = Users:create({
 		name = admin.name,
 		tag = admin.tag,
 		email = admin.email,
@@ -171,6 +179,53 @@ app:match("/api/create_db", function(self)
 		creation_time = 0,
 		description = "",
 	})
+
+	local community = Communities:create({
+		name = "Community",
+		alias = "???",
+		link = "https://soundsphere.xyz",
+		short_description = "Short descr.",
+		description = "Long description",
+		banner = "",
+		is_public = true,
+	})
+
+	Community_users:create({
+		community_id = community.id,
+		user_id = user.id,
+		sender_id = user.id,
+		role = Roles:for_db("creator"),
+		accepted = true,
+		created_at = os.time(),
+		message = "",
+	})
+
+	local difftable = Difftables:create({
+		name = "Difficulty table",
+		link = "https://soundsphere.xyz",
+		description = "Description",
+		owner_community_id = community.id,
+	})
+
+	local leaderboard = Leaderboards:create({
+		name = "Leaderboard",
+		description = "Description",
+		banner = "",
+	})
+
+	Community_leaderboards:create({
+		community_id = community.id,
+		leaderboard_id = leaderboard.id,
+		is_owner = true,
+		sender_id = user.id,
+		accepted = true,
+		created_at = os.time(),
+		message = "",
+	})
+
+	leaderboard_c.update_inputmodes(leaderboard.id, {"10key"})
+	leaderboard_c.update_difftables(leaderboard.id, {difftable})
+	leaderboard_c.update_modifiers(leaderboard.id, {{name = "Automap", value = "4 to 10", rule = "required"}})
 end)
 
 function app:handle_error(err, trace)
