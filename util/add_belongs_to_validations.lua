72306b9439a01a96fc66cabbf5cbf96041df5efe
semyon422 <semyon422@users.noreply.github.com>
2021-12-29 12:03:32 +0500

    Implement nested relations loading

--- util/add_belongs_to_validations.lua
+++ util/add_belongs_to_validations.lua
@@ -1,9 +1,18 @@
-return function(relations, validations)
+local models = require("models")
+
+local function add(relations, validations, prefix)
+	prefix = prefix or ""
 	local validations = validations or {}
 	for _, relation in ipairs(relations) do
 		if relation.belongs_to then
-			table.insert(validations, {relation[1], type = "boolean", optional = true})
+			table.insert(validations, {prefix .. relation[1], type = "boolean", optional = true})
+			local subrelations = models[relation.belongs_to].relations
+			if subrelations then
+				add(subrelations, validations, relation[1] .. "_")
+			end
 		end
 	end
 	return validations
 end
+
+return add
--- util/get_relatives.lua
+++ util/get_relatives.lua
@@ -1,12 +1,23 @@
-return function(object, params, to_name)
+local function get(object, params, to_name)
 	local relations = object.__class.relations
 
 	for _, relation in ipairs(relations) do
 		if params[relation[1]] then
 			local relative = object["get_" .. relation[1]](object)
-			if relative and to_name and relative.to_name then
-				relative:to_name()
+			if relative then
+				if to_name and relative.to_name then
+					relative:to_name()
+				end
+				local new_params = {}
+				for key, value in pairs(params) do
+					if type(key) == "string" and key:find("^" .. relation[1] .. "_.+$") then
+						new_params[key:match("^" .. relation[1] .. "_(.+)$")] = value
+					end
+				end
+				get(relative, new_params, to_name)
 			end
 		end
 	end
 end
+
+return get
