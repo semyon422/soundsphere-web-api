636d6a8f68cda30e4ca18aff3c4bb8934ecdedbb
semyon422 <semyon422@users.noreply.github.com>
2022-01-04 22:01:17 +0500

    Fix json_params for Content-Type: application/json

--- util/json_params.lua
+++ util/json_params.lua
@@ -1,9 +1,19 @@
 local json = require("cjson")
-local json_params = require("lapis.application").json_params
 
 return function(fn)
-	json_params(fn)
 	return function(self, ...)
+		local content_type = self.req.headers["content-type"]
+		if content_type then
+			if string.find(content_type:lower(), "application/json", nil, true) then
+				local body = self.req:read_body_as_string()
+				local success, obj_or_err = pcall(function()
+					return json.decode(body)
+				end)
+				if success then
+					self.__class.support.add_params(self, obj_or_err, "json")
+				end
+			end
+		end
 		local input = self.params.json_params
 		local content
 		if type(input) == "table" and input.filename ~= "" and input.content ~= "" then
