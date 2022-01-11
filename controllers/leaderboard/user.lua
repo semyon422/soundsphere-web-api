f8f0b5cc1371bb924b8ad9a07814e488127bee1e
semyon422 <semyon422@users.noreply.github.com>
2022-01-11 22:05:36 +0500

    Improve leaderboard.users, rework loeaderboard.user

--- controllers/leaderboard/user.lua
+++ controllers/leaderboard/user.lua
@@ -1,33 +1,33 @@
 local Leaderboard_users = require("models.leaderboard_users")
 local Controller = require("Controller")
+local util = require("util")
 
 local leaderboard_user_c = Controller:new()
 
 leaderboard_user_c.path = "/leaderboards/:leaderboard_id[%d]/users/:user_id[%d]"
-leaderboard_user_c.methods = {"PUT", "DELETE"}
+leaderboard_user_c.methods = {"GET", "PATCH"}
 
-leaderboard_user_c.context.PUT = {"leaderboard_user", "request_session"}
-leaderboard_user_c.policies.PUT = {{"authenticated"}}
-leaderboard_user_c.PUT = function(self)
-	local params = self.params
+leaderboard_user_c.context.GET = {"leaderboard_user", "request_session"}
+leaderboard_user_c.policies.GET = {{"authenticated", "context_loaded"}}
+leaderboard_user_c.validations.GET = util.add_belongs_to_validations(Leaderboard_users.relations)
+leaderboard_user_c.GET = function(self)
     local leaderboard_user = self.context.leaderboard_user
-    if not leaderboard_user then
-        leaderboard_user = Leaderboard_users:create({
-			leaderboard_id = params.leaderboard_id,
-			user_id = params.user_id,
-		})
-    end
 
 	return {json = {leaderboard_user = leaderboard_user}}
 end
 
-leaderboard_user_c.context.DELETE = {"leaderboard_user", "request_session"}
-leaderboard_user_c.policies.DELETE = {{"authenticated", "context_loaded"}}
-leaderboard_user_c.DELETE = function(self)
+leaderboard_user_c.context.PATCH = {"leaderboard_user", "request_session"}
+leaderboard_user_c.policies.PATCH = {{"authenticated", "context_loaded"}}
+leaderboard_user_c.validations.PATCH = {
+	{"active", type = "boolean", optional = true},
+}
+leaderboard_user_c.PATCH = function(self)
     local leaderboard_user = self.context.leaderboard_user
-    leaderboard_user:delete()
 
-	return {status = 204}
+	leaderboard_user.active = self.params.active
+    leaderboard_user:update("active")
+
+	return {json = {leaderboard_user = leaderboard_user}}
 end
 
 return leaderboard_user_c
--- controllers/leaderboard/users.lua
+++ controllers/leaderboard/users.lua
@@ -8,19 +8,77 @@ local leaderboard_users_c = Controller:new()
 leaderboard_users_c.path = "/leaderboards/:leaderboard_id[%d]/users"
 leaderboard_users_c.methods = {"GET"}
 
+leaderboard_users_c.get_users = function(self)
+	local params = self.params
+	local db = Leaderboard_users.db
+
+	local clause_table = {"lu"}
+	local where_table = {"lu.active = true", "lu.leaderboard_id = ?"}
+	local fields = {"lu.*"}
+	local orders = {}
+	local opts = {params.leaderboard_id}
+
+	if params.community_id then
+		table.insert(clause_table, "inner join community_users cu on lu.user_id = cu.user_id")
+		table.insert(where_table, "cu.accepted = true")
+		table.insert(where_table, "cu.community_id = ?")
+		table.insert(opts, params.community_id)
+	end
+	if params.search then
+		table.insert(clause_table, "inner join users u on lu.user_id = u.id")
+		table.insert(where_table, "(" .. util.db_search(db, params.search, "name") .. ")")
+	end
+	table.insert(orders, "lu.total_rating desc")
+	table.insert(orders, "lu.user_id asc")
+
+	table.insert(clause_table, util.db_where(util.db_and(where_table)))
+	table.insert(clause_table, "order by " .. table.concat(orders, ", "))
+
+	local per_page = params.per_page or 10
+	local page_num = params.page_num or 1
+	local clause = db.interpolate_query(
+		table.concat(clause_table, " "),
+		unpack(opts)
+	)
+
+	local paginator = Leaderboard_users:paginated(clause, {
+		per_page = per_page,
+		fields = table.concat(fields, ", "),
+	})
+	local leaderboard_users = params.get_all and paginator:get_all() or paginator:get_page(page_num)
+
+	for i, leaderboard_user in ipairs(leaderboard_users) do
+		leaderboard_user.rank = (page_num - 1) * per_page + i
+	end
+
+	return leaderboard_users, clause
+end
+
 leaderboard_users_c.policies.GET = {{"permit"}}
 leaderboard_users_c.validations.GET = {
 	require("validations.no_data"),
+	require("validations.per_page"),
+	require("validations.page_num"),
+	require("validations.get_all"),
+	require("validations.search"),
+	{"community_id", exists = true, type = "number", optional = true, default = ""},
 }
-leaderboard_users_c.validations.GET = util.add_belongs_to_validations(Leaderboard_users.relations)
+util.add_belongs_to_validations(Leaderboard_users.relations, leaderboard_users_c.validations.GET)
 leaderboard_users_c.GET = function(self)
 	local params = self.params
-    local leaderboard_users = Leaderboard_users:find_all({params.leaderboard_id}, "leaderboard_id")
+
+	local leaderboard_users, filtered_clause = leaderboard_users_c.get_users(self)
+
+	local db = Leaderboard_users.db
+	local total_clause = db.encode_clause({
+		leaderboard_id = params.leaderboard_id,
+		active = true,
+	})
 
 	if params.no_data then
 		return {json = {
-			total = #leaderboard_users,
-			filtered = #leaderboard_users,
+			total = tonumber(Leaderboard_users:count(total_clause)),
+			filtered = tonumber(util.db_count(Leaderboard_users, filtered_clause)),
 		}}
 	end
 
@@ -28,8 +86,8 @@ leaderboard_users_c.GET = function(self)
 	util.recursive_to_name(leaderboard_users)
 
 	return {json = {
-		total = #leaderboard_users,
-		filtered = #leaderboard_users,
+		total = tonumber(Leaderboard_users:count(total_clause)),
+		filtered = tonumber(util.db_count(Leaderboard_users, filtered_clause)),
 		leaderboard_users = leaderboard_users,
 	}}
 end
