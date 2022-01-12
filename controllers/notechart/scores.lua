76b417400f16015ee1cfcf87d2cc47b3bc2f86ae
semyon422 <semyon422@users.noreply.github.com>
2022-01-12 21:32:56 +0500

    Add filters for notechart.scores

--- controllers/notechart/scores.lua
+++ controllers/notechart/scores.lua
@@ -10,7 +10,7 @@ local notechart_scores_c = Controller:new()
 notechart_scores_c.path = "/notecharts/:notechart_id[%d]/scores"
 notechart_scores_c.methods = {"GET"}
 
-local function get_relations_scores(params, relationtype, mutual)
+notechart_scores_c.get_relations_scores = function(params, relationtype, mutual)
 	local user_id = tonumber(params[relationtype .. "s"])
 
 	local user_ids = {user_id}
@@ -41,28 +41,82 @@ local function get_relations_scores(params, relationtype, mutual)
 	return scores
 end
 
+notechart_scores_c.get_scores = function(self)
+	local params = self.params
+	local db = Scores.db
+
+	local clause_table = {"s"}
+	local where_table = {"s.notechart_id = ?", "s.is_valid = ?", "s.is_complete = ?"}
+	local fields = {"s.*"}
+	local orders = {}
+	local opts = {params.notechart_id, true, true}
+
+	if params.leaderboard_id then
+		table.insert(clause_table, "inner join leaderboard_users lu on s.user_id = lu.user_id")
+		table.insert(where_table, "lu.active = true")
+		table.insert(where_table, "lu.leaderboard_id = ?")
+		table.insert(opts, params.leaderboard_id)
+	end
+	if params.search then
+		table.insert(clause_table, "inner join users u on s.user_id = u.id")
+		table.insert(where_table, util.db_search(db, params.search, "u.name"))
+	end
+	table.insert(orders, "s.rating desc")
+
+	table.insert(clause_table, util.db_where(util.db_and(where_table)))
+	table.insert(clause_table, "order by " .. table.concat(orders, ", "))
+
+	local per_page = params.per_page or 10
+	local page_num = params.page_num or 1
+	local clause = db.interpolate_query(
+		table.concat(clause_table, " "),
+		unpack(opts)
+	)
+
+	local paginator = Scores:paginated(clause, {
+		per_page = per_page,
+		fields = table.concat(fields, ", "),
+	})
+	local scores = params.get_all and paginator:get_all() or paginator:get_page(page_num)
+
+	return scores, clause
+end
+
 notechart_scores_c.policies.GET = {{"permit"}}
 notechart_scores_c.validations.GET = {
 	{"rivals", type = "boolean", optional = true},
 	{"friends", type = "boolean", optional = true},
+	require("validations.no_data"),
+	require("validations.per_page"),
+	require("validations.page_num"),
+	require("validations.get_all"),
+	require("validations.search"),
+	{"leaderboard_id", exists = true, type = "number", optional = true, default = ""},
 }
 util.add_belongs_to_validations(Scores.relations, notechart_scores_c.validations.GET)
 notechart_scores_c.GET = function(self)
 	local params = self.params
 	local scores
-	local notechart_id = params.notechart_id
+
+	local clause
 	if params.rivals then
-		scores = get_relations_scores(params, "rival")
+		scores = notechart_scores_c.get_relations_scores(params, "rival")
 	elseif params.friends then
-		scores = get_relations_scores(params, "friend", true)
+		scores = notechart_scores_c.get_relations_scores(params, "friend", true)
 	else
-		scores = Scores:find_all({notechart_id}, "notechart_id")
+		scores, clause = notechart_scores_c.get_scores(self)
 	end
+
+	local total_clause = Scores.db.encode_clause({
+		notechart_id = params.notechart_id,
+		is_valid = params.is_valid,
+		is_complete = params.is_complete,
+	})
 
 	if params.no_data then
 		return {json = {
-			total = #scores,
-			filtered = #scores,
+			total = tonumber(Scores:count(total_clause)),
+			filtered = not clause and #scores or tonumber(util.db_count(Scores, clause)),
 		}}
 	end
 
@@ -70,8 +124,8 @@ notechart_scores_c.GET = function(self)
 	util.recursive_to_name(scores)
 
 	return {json = {
-		total = #scores,
-		filtered = #scores,
+		total = tonumber(Scores:count(total_clause)),
+		filtered = not clause and #scores or tonumber(util.db_count(Scores, clause)),
 		scores = scores,
 	}}
 end
