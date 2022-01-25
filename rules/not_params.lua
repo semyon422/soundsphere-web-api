4ca1983c157a822de094ff6d34aeb4e3040b42c5
semyon422 <semyon422@users.noreply.github.com>
2022-01-25 23:37:44 +0500

    Delete old debug prints

--- rules/not_params.lua
+++ rules/not_params.lua
@@ -3,10 +3,8 @@ local Rule = require("abac.Rule")
 local rule = Rule:new()
 
 function rule:condition(request)
-	print(123123)
 	local not_params = assert(self.not_params)
 	if type(not_params) == "string" then
-		print(not request.params[not_params])
 		return not request.params[not_params]
 	end
 	for _, v in ipairs(not_params) do
