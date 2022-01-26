2a277c6ceb36f4770b470d922690463a015b1a2a
semyon422 <semyon422@users.noreply.github.com>
2022-01-26 16:21:19 +0500

    More strict session check

--- rules/authed.lua
+++ rules/authed.lua
@@ -3,7 +3,13 @@ local Rule = require("abac.Rule")
 local rule = Rule:new()
 
 function rule:condition(request)
-	return request.context.request_session.active
+	local cs = request.context.request_session
+	local rs = request.session
+	return
+		cs.active and
+		cs.id == rs.id and
+		cs.user_id == rs.user_id and
+		cs.created_at == rs.created_at
 end
 
 rule.effect = "permit"
