6f42b455c0a281d3e7712aa3ffe63018203df6fa
semyon422 <semyon422@users.noreply.github.com>
2022-02-08 18:54:31 +0500

    Allow logout if no session found

--- controllers/auth/logout.lua
+++ controllers/auth/logout.lua
@@ -1,21 +1,21 @@
 local Controller = require("Controller")
+local login_c = require("controllers.auth.login")
 
 local logout_c = Controller:new()
 
 logout_c.path = "/auth/logout"
 logout_c.methods = {"POST"}
 
-logout_c.context.POST = {"request_session"}
-logout_c.policies.POST = {{"authed"}}
+logout_c.context.POST = {{"request_session", optional = true}}
+logout_c.policies.POST = {{"permit"}}
 logout_c.POST = function(self)
 	local session = self.context.request_session
 
-	session.active = false
-	session:update("active")
-
-	for key, value in pairs(session) do
-		self.session[key] = nil
+	if session then
+		session.active = false
+		session:update("active")
 	end
+	login_c.copy_session({}, self.session)
 
 	return {}
 end
