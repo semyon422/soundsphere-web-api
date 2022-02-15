586cb3a28d379df6e4b3bb1b9c2980ce1972773e
semyon422 <semyon422@users.noreply.github.com>
2022-02-15 23:22:19 +0500

    Add role in user.leaderboards in leaderboard is loaded

--- controllers/user/leaderboards.lua
+++ controllers/user/leaderboards.lua
@@ -9,6 +9,7 @@ local user_leaderboards_c = Controller:new()
 user_leaderboards_c.path = "/users/:user_id[%d]/leaderboards"
 user_leaderboards_c.methods = {"GET"}
 
+user_leaderboards_c.context.GET = {"user", "user_communities"}
 user_leaderboards_c.policies.GET = {{"permit"}}
 user_leaderboards_c.validations.GET = {
 	require("validations.no_data"),
@@ -30,6 +31,18 @@ user_leaderboards_c.GET = function(self)
 	preload(leaderboard_users, util.get_relatives_preload(Leaderboard_users, params))
 	util.relatives_preload_field(leaderboard_users, "leaderboard", Leaderboards, params)
 	util.recursive_to_name(leaderboard_users)
+
+	local user = self.context.user
+	if leaderboard_users[1] and leaderboard_users[1].leaderboard then
+		for _, leaderboard_user in ipairs(leaderboard_users) do
+			local community_user = user.communities:select({
+				community_id = leaderboard_user.leaderboard.owner_community_id
+			})[1]
+			if community_user then
+				leaderboard_user.role = community_user.role
+			end
+		end
+	end
 
 	return {json = {
 		total = #leaderboard_users,
