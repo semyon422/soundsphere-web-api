14ea829b9391d3d3c06cc4388ec1951e3732c113
semyon422 <semyon422@users.noreply.github.com>
2022-02-17 16:15:47 +0500

    Better quick login

--- controllers/auth/quick.lua
+++ controllers/auth/quick.lua
@@ -1,5 +1,4 @@
 local rand = require("openssl.rand")
-local users = require("models.users")
 local Quick_logins = require("models.quick_logins")
 local login_c = require("controllers.auth.login")
 local Controller = require("Controller")
@@ -9,69 +8,77 @@ local Filehash = require("util.filehash")
 local quick_c = Controller:new()
 
 quick_c.path = "/auth/quick"
-quick_c.methods = {"GET", "POST"}
+quick_c.methods = {"GET", "POST", "PUT"}
 
 local new_key = function()
 	return rand.bytes(16)
 end
-
-local messages = {}
-messages.not_allowed = "Quick login is not allowed"
-messages.success = "Success"
 
 quick_c.policies.GET = {{"permit"}}
 quick_c.validations.GET = {
 	{"key", type = "string", optional = true},
 }
 quick_c.GET = function(self)
-	local params = self.params
+	local key = self.params.key
+	if key and #key > 0 then
+		return {json = {
+			message = "Key passed, no new key generated",
+			key = key,
+		}}
+	end
+
 	local ip = self.context.ip
 	local time = os.time()
 	local quick_login = Quick_logins:find({ip = Ip:for_db(ip)})
 
 	if not quick_login then
-		if params.key then
-			return {json = {message = messages.not_allowed}}
-		end
-
-		local key = new_key()
+		key = new_key()
 		Quick_logins:create({
 			ip = Ip:for_db(ip),
 			key = key,
 			next_update_time = time + 5 * 60,
 		})
-
-		return {json = {key = Filehash:to_name(key)}}
-	end
-
-	if quick_login.next_update_time < time or not params.key then
-		local key = new_key()
+	else
+		key = new_key()
 		quick_login.key = key
 		quick_login.next_update_time = time + 5 * 60
 		quick_login.complete = false
 		quick_login:update("key", "next_update_time", "complete")
-
-		return {json = {key = Filehash:to_name(key)}}
 	end
 
-	params.key = Filehash:for_db(params.key)
-	if quick_login.key ~= params.key or not quick_login.complete then
-		return {json = {message = messages.not_allowed}}
+	return {json = {key = Filehash:to_name(key)}}
+end
+
+quick_c.policies.PUT = {{"permit"}}
+quick_c.validations.PUT = {
+	{"key", type = "string"},
+}
+quick_c.PUT = function(self)
+	local ip = self.context.ip
+	local quick_login = Quick_logins:find({ip = Ip:for_db(ip)})
+
+	if not quick_login then
+		return {status = 404, json = {message = "Quick login not found"}}
+	elseif quick_login.next_update_time < os.time() then
+		return {status = 400, json = {message = "Quick login expired"}}
 	end
 
-	local user = users:find(quick_login.user_id)
-	if user then
-		quick_login:delete()
-		local token, payload = login_c.new_token(user, ip)
-		login_c.add_user_location(user, ip, false)
-
-		return {json = {
-			token = token,
-			session = payload,
-		}}
+	local key = Filehash:for_db(self.params.key)
+	if quick_login.key ~= key then
+		return {status = 401, json = {message = "Wrong key"}}
+	elseif not quick_login.complete then
+		return {status = 403, json = {message = "Not complete"}}
 	end
 
-	return {json = {message = messages.not_allowed}}
+	local user = quick_login:get_user()
+	quick_login:delete()
+	local token, payload = login_c.new_token(user, ip)
+	login_c.add_user_location(user, ip, false)
+
+	return {json = {
+		token = token,
+		session = payload,
+	}}
 end
 
 quick_c.context.POST = {"request_session"}
@@ -82,25 +89,23 @@ quick_c.validations.POST = {
 quick_c.POST = function(self)
 	local key = self.params.key
 
-	if not key then
-		return {json = {message = messages.not_allowed}}
-	end
-
 	key = Filehash:for_db(key)
 	local quick_login = Quick_logins:find({
 		ip = Ip:for_db(self.context.ip),
 		key = key
 	})
 
-	if not quick_login or quick_login.complete then
-		return {json = {message = messages.not_allowed}}
+	if not quick_login then
+		return {status = 404, json = {message = "Quick login not found"}}
+	elseif quick_login.complete then
+		return {status = 403, json = {message = "Complete"}}
 	end
 
 	quick_login.user_id = self.session.user_id
 	quick_login.complete = true
 	quick_login:update("user_id", "complete")
 
-	return {json = {message = messages.success}}
+	return {json = {message = "Success"}}
 end
 
 return quick_c
