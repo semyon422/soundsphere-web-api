3c55997b5bfdc23d6bee65260d0cdfc3592affe0
semyon422 <semyon422@users.noreply.github.com>
2022-08-17 15:55:15 +0500

    Fix Filehash bug when converting twice

--- util/filehash.lua
+++ util/filehash.lua
@@ -11,10 +11,16 @@ function Filehash.sum_to_name(self, s)
 end
 
 function Filehash.for_db(self, hex)
+	if #hex == 16 then
+		return hex
+	end
     return (hex:gsub("..", function(cc) return string.char(tonumber(cc, 16) or 0) end))
 end
 
 function Filehash.to_name(self, data)
+	if #data == 32 then
+		return data
+	end
 	return (data:gsub(".", function(c) return ("%02x"):format(c:byte()) end))
 end
 
